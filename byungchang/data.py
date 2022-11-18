b6b8f2f582a2c05c25d54ca3c6b9acc5dcb8bfe8
Byungchang So <sinwall@snu.ac.kr>
2022-11-18 16:21:40 +0900

    up to first meeting

--- byungchang/data.py
+++ byungchang/data.py
@@ -1,12 +1,18 @@
 import numpy as np
 import pandas as pd
 
-input_path = '../input/서울의대법의학교실_Ancestry SNP Panel Genotype raw data'
+input_path = '../../input/서울의대법의학교실_Ancestry SNP Panel Genotype raw data'
+regions = ['NEA', 'SEA', 'SWA']
+popls = [
+    'CHS', 'CHB', 'CDX', 'JPT', 'KOR', 
+    'KHV', 'VNK', 'MYA',
+    'BEB', 'ITU', 'GIH', 'STU', 'NP', 'IN', 'PJL', 'PT'
+]
 
 def load_data():
     df_NEA = pd.read_csv(f'{input_path}/서울의대법의학교실_Ancestry SNP Panel Genotype raw data_2022_NEA.csv').dropna(how='all')
     df_NEA.iloc[545:554, 2:] = df_NEA.iloc[545:554, 1:-1]
-    df_NEA.iloc[545:554, 2] = 'NN'
+    df_NEA.iloc[545:554, 1] = 'NN'
 
     df_SEA = pd.read_csv(f'{input_path}/서울의대법의학교실_Ancestry SNP Panel Genotype raw data_2022_SEA.csv').dropna(how='all')
     df_SWA = pd.read_csv(f'{input_path}/서울의대법의학교실_Ancestry SNP Panel Genotype raw data_2022_SWA.csv').dropna(how='all')
@@ -21,5 +27,6 @@ def load_data():
         ], axis=0), name='region'),
         df['SNU-ID'].str.split('-').apply(lambda x:x[1]).rename('popl')
     ], axis=1).astype('string')
+    df = df.sort_values(by='popl', key=lambda ser:ser.map(lambda x: popls.index(x)))
 
     return df
